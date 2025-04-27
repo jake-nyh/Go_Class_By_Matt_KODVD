8f453cf73b6488fb20709c92a03294fedac20665
Nyan Ye Htet <nyanyairhtet@ayainnovations.com>
2025-04-27 22:59:07 +0630

    add basic types

---
+++ basic-types/basicTypes.go
@@ -0,0 +1,11 @@
+package BasicTypes
+
+import "fmt"
+
+func BasicTypes() {
+	a := 2
+	b := 3.1
+
+	fmt.Printf("a: %T %v\n", a, a)
+	fmt.Printf("b: %T %v\n", b, b)
+}
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 
+	BasicTypes "github.com/jake-nyh/Go_Class_By_Matt_KODVD/basic-types"
 	simpleExample "github.com/jake-nyh/Go_Class_By_Matt_KODVD/simple-example"
 )
 
 func main() {
 	fmt.Println("Hello World")
 	simpleExample.SimpleExample()
+	BasicTypes.BasicTypes()
 }
