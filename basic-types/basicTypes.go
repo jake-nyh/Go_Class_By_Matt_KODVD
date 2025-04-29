f8bef288724a7e45f41b28d2834d4b27559de51d
Nyan Ye Htet <nyanyairhtet@gmail.com>
2025-04-29 23:31:00 +0630

    add basic types

--- basic-types/basicTypes.go
+++ basic-types/basicTypes.go
@@ -6,6 +6,11 @@ func BasicTypes() {
 	a := 2
 	b := 3.1
 
-	fmt.Printf("a: %T %v\n", a, a)
+	fmt.Printf("a: %T %[1]v\n", a)
 	fmt.Printf("b: %T %v\n", b, b)
+
+	a = int(b)
+
+	fmt.Printf("a: %T %[1]v\n", a, a)
+
 }
